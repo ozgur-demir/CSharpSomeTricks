25c6e7c5ee72e19e4321835f2347f65f98bb4aee
ozgur-demir <info@csyolu.com>
2022-05-19 21:53:25 +0300

    Override virtual method example added.

---
+++ OverrideVirtualMethod/Program.cs
@@ -0,0 +1,15 @@
+﻿//Override virtual method
+
+Customer c = new Customer("Ozgur", "Demir");
+Console.WriteLine(c);
+
+class Customer
+{
+    private string Name { get; set; }
+    private string Surname { get; set; }
+    public Customer(string Name, string Surname) { this.Name = Name; this.Surname = Surname; }
+    public override string ToString()
+    {
+        return $"{Name} {Surname}";
+    }
+}
