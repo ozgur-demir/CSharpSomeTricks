283baa8bd8236dbebdaa27e33263823f1cc4d5fd
ozgur-demir <info@csyolu.com>
2022-05-19 14:39:58 +0300

    You can also use it with Lambda and Generic Delegate :: Func<> Predicate<>

--- EventInClassExample(Delegates)/Program.cs
+++ EventInClassExample(Delegates)/Program.cs
@@ -2,29 +2,22 @@
 //Using custom delegate
 Car car = new Car();
 car.Color = "Blue";
-car.speedEvent += C_speedEvent;
+car.speedEvent += (speedval) => { Console.WriteLine("Speed limit exceeded::::: Current Speed=" + speedval); return true; };
 for (int i = 5; i < 90; i += 5)
 {
     System.Threading.Thread.Sleep(300);
     car.Speed = i;
     Console.WriteLine("The vehicle is accelerating:" + car.Speed);
 }
-static void C_speedEvent(int speedValue)
-{
-    Console.WriteLine($"Speed limit exceeded::::: Current Speed={speedValue}");
-}
-public delegate void speedEvent(int speed);
+
 public class Car
 {
-    public event speedEvent speedEvent;
+    public event Func<int,bool> speedEvent;  //Predicate<int>
     private int _speed;
     public string? Color { get; set; }
     public int Speed
     {
-        get
-        {
-            return _speed;
-        }
+        get => _speed;
         set
         {
             if (value >= 80 && speedEvent != null)
