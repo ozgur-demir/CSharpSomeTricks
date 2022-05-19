49d454438772eab5d9564be851ebe144934980c4
ozgur-demir <info@csyolu.com>
2022-05-19 15:44:31 +0300

    Value/Referance Type Diffrence Example Added

---
+++ ValueType-ReferanceType-Diffrence/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int number = 5;
+Console.WriteLine("Default Number:"+number);
+EditAsValueType(number);
+Console.WriteLine("After Edit(ValueType):" + number);
+EditAsReferanceType(ref number);
+Console.WriteLine("After Edit(ReferanceType):" + number);
+
+
+
+static void EditAsValueType(int number)
+{
+    number = number + 5;
+}
+static void EditAsReferanceType(ref int number)
+{
+    number = number + 5;
+}
